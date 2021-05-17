9fd687398b05fc6419e77399e254237194ba1c13
Murodjon <murodbek.0338@gmail.com>
2021-05-17 14:07:15 +0500

    contact front ready

--- resources/views/cantact.blade.php
+++ resources/views/cantact.blade.php
@@ -7,8 +7,8 @@
 				<div class="col-md-8 col-md-offset-2 text-center">
 					<div class="display-t">
 						<div class="display-tc animate-box" data-animate-effect="fadeIn">
-							<h1>Contact Us</h1>
-							<h2>Free html5 templates by <a href="https://themewagon.com/theme_tag/free/" target="_blank">Themewagon</a></h2>
+							<h1>Biz bilan ALOQA</h1>
+							
 						</div>
 					</div>
 				</div>
@@ -22,52 +22,52 @@
 				<div class="col-md-5 col-md-push-1 animate-box">
 					
 					<div class="fh5co-contact-info">
-						<h3>Contact Information</h3>
+						<h3>Bizning manzil:</h3>
 						<ul>
-							<li class="address">198 West 21th Street, <br> Suite 721 New York NY 10016</li>
-							<li class="phone"><a href="tel://1234567920">+ 1235 2355 98</a></li>
-							<li class="email"><a href="mailto:info@yoursite.com">info@yoursite.com</a></li>
-							<li class="url"><a href="http://gettemplates.co">gettemplates.co</a></li>
+							<li class="address">220100, Xorazm viloyati, Urganch shahri, <br>  Al-Xorazmiy ko'chasi, 110-uy</li>
+							<li class="phone"><a href="tel://904333699">+99890 433 36 99, <br> </a><a href="tel://937462676">+99893 746 26 76</a></li>
+							<li class="email"><a href="mailto:murodbek.0338@gmail.com">murodbek.0338@gmail.com <br></a><a href="mailto:boburbekrustamov1999@gmail.com">boburbekrustamov1999@gmail.com</a></li>
+							
 						</ul>
 					</div>
 
 				</div>
 				<div class="col-md-6 animate-box">
-					<h3>Get In Touch</h3>
+					<h3>Aloqa qiling</h3>
 					<form action="#">
 						<div class="row form-group">
 							<div class="col-md-6">
 								<!-- <label for="fname">First Name</label> -->
-								<input type="text" id="fname" class="form-control" placeholder="Your firstname">
+								<input type="text" id="fname" class="form-control" placeholder="Isminggiz">
 							</div>
 							<div class="col-md-6">
 								<!-- <label for="lname">Last Name</label> -->
-								<input type="text" id="lname" class="form-control" placeholder="Your lastname">
+								<input type="text" id="lname" class="form-control" placeholder="Familiyangiz">
 							</div>
 						</div>
 
 						<div class="row form-group">
 							<div class="col-md-12">
 								<!-- <label for="email">Email</label> -->
-								<input type="text" id="email" class="form-control" placeholder="Your email address">
+								<input type="text" id="email" class="form-control" placeholder="Pochta manzilinggiz">
 							</div>
 						</div>
 
 						<div class="row form-group">
 							<div class="col-md-12">
 								<!-- <label for="subject">Subject</label> -->
-								<input type="text" id="subject" class="form-control" placeholder="Your subject of this message">
+								<input type="text" id="subject" class="form-control" placeholder="Xabaringgizning mavzusi">
 							</div>
 						</div>
 
 						<div class="row form-group">
 							<div class="col-md-12">
 								<!-- <label for="message">Message</label> -->
-								<textarea name="message" id="message" cols="30" rows="10" class="form-control" placeholder="Say something about us"></textarea>
+								<textarea name="message" id="message" cols="30" rows="10" class="form-control" placeholder="Xabaringgizni kiriting"></textarea>
 							</div>
 						</div>
 						<div class="form-group">
-							<input type="submit" value="Send Message" class="btn btn-primary">
+							<input type="submit" value="Xabarni jo'natish" class="btn btn-primary">
 						</div>
 
 					</form>		
